0b42f5a595eec000ab49b4573df143ee10f35695
Dinh Y Thuc <dinhythuc240594@gmail.com>
2025-11-11 23:24:57 +0700

    update order

--- src/main/java/controller/OrderServerlet.java
+++ src/main/java/controller/OrderServerlet.java
@@ -5,7 +5,23 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import model.OrderDAO;
+import model.Order_FoodDAO;
+import model.PageRequest;
+import repository.OrderRepository;
+import repository.Order_FoodRepository;
+import repositoryimpl.OrderRepositoryImpl;
+import repositoryimpl.Order_FoodRepositoryImpl;
+import utils.DataSourceUtil;
+import utils.RequestUtil;
+
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.sql.DataSource;
 
 /**
  * Servlet implementation class OrderServerlet
@@ -22,12 +38,48 @@ public class OrderServerlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
+    private OrderRepository orderRepository;
+    private Order_FoodRepository orderFoodRepository;
+    private int PAGE_SIZE = 25;
+
+    @Override
+    public void init() throws ServletException {
+    	DataSource ds = DataSourceUtil.getDataSource();
+        orderRepository = new OrderRepositoryImpl(ds);
+        orderFoodRepository = new Order_FoodRepositoryImpl(ds);
+    }
+    
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		 HttpSession session = request.getSession(false);
+	        if (session == null || session.getAttribute("userId") == null) {
+	        	response.sendRedirect(request.getContextPath() + "/login");
+	            return;
+	        }
+
+	        int userId = (int) session.getAttribute("userId");
+
+//			String keyword = RequestUtil.getString(request, "keyword", "");
+//			String sortField = RequestUtil.getString(request, "sortField", "name");
+//			String orderField = RequestUtil.getString(request, "orderField", "ASC");
+//			int page = RequestUtil.getInt(request, "page", 1);
+//	        
+//	        PageRequest pageReq = new PageRequest(page, PAGE_SIZE, sortField, orderField, keyword);
+//	        List<OrderDAO> orders = this.orderRepository.findAll(pageReq);
+
+	        List<OrderDAO> orders = this.orderRepository.findByUserId(userId);	        
+	        Map<Integer, List<Order_FoodDAO>> orderFoodMap = new HashMap<>();
+	        for (OrderDAO order : orders) {
+	            List<Order_FoodDAO> items = orderFoodRepository.findByOrderId(order.getId());
+	            orderFoodMap.put(order.getId(), items);
+	        }
+	        
+	        request.setAttribute("orders", orders);
+	        request.setAttribute("orderFoodMap", orderFoodMap);
+
+	        request.getRequestDispatcher("order.jsp").forward(request, response);
 	}
 
 	/**
